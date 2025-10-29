f63aa741e79afeb198338d553c11e3dd4223638e
gkdbwjd0820 <gkdbwjd0820@gmail.com>
2025-10-29 21:56:47 +0900

    섹션3,4 했음

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
 
+    var swiper = new Swiper(".sec_1_2", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
   
 });
