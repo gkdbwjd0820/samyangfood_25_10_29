eb8b05e073698051a0d9493b6cba50930079e27f
gkdbwjd0820 <gkdbwjd0820@gmail.com>
2025-10-30 20:24:15 +0900

    모바일버전 끝냄

--- js/main.js
+++ js/main.js
@@ -18,8 +18,8 @@ $(document).ready(function () {
   });
 
   var swiper = new Swiper(".con_2_wrap", {
-    slidesPerView: 1.8,
-    spaceBetween: 20,
+    slidesPerView: 1.6,
+    spaceBetween: 16,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -33,5 +33,34 @@ $(document).ready(function () {
       el: ".swiper-pagination",
       clickable: true,
     },
+  });
+
+
+ // 모든 faq 영역 안의 plus, minus 아이콘 선택
+  const plusIcons = document.querySelectorAll(".plus");
+  const minusIcons = document.querySelectorAll(".minus");
+
+  plusIcons.forEach((plus) => {
+    plus.addEventListener("click", function (event) {
+      event.preventDefault(); // ★ 링크 기본 동작 막기
+
+      const parent = plus.parentElement;
+      const minus = parent.querySelector(".minus");
+
+      minus.style.display = "block";
+      plus.style.display = "none";
+    });
+  });
+
+  minusIcons.forEach((minus) => {
+    minus.addEventListener("click", function (event) {
+      event.preventDefault(); // ★ 링크 기본 동작 막기
+
+      const parent = minus.parentElement;
+      const plus = parent.querySelector(".plus");
+
+      minus.style.display = "none";
+      plus.style.display = "block";
+    });
   });
 });
