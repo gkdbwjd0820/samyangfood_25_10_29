d64c00402ae08d0a60fccbdf717adfefb8d5312f
gkdbwjd0820 <gkdbwjd0820@gmail.com>
2025-10-30 11:15:39 +0900

    섹션1_2 호버빼고 끝

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  var swiper = new Swiper(".sec_1_2", {
+    slidesPerView: 3.8,
+    spaceBetween: 8,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 
-    var swiper = new Swiper(".sec_1_2", {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
-    var swiper = new Swiper(".sec_5", {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
-  
+  var swiper = new Swiper(".sec_1_2_food", {
+    slidesPerView: 1.8,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  var swiper = new Swiper(".sec_5", {
+    slidesPerView: 3.8,
+    spaceBetween: 8,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 });
