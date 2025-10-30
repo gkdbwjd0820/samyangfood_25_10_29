4389f58b5ebe70f2d71b7d4bee5b5bac39b1a3f1
gkdbwjd0820 <gkdbwjd0820@gmail.com>
2025-10-30 12:52:42 +0900

    섹션2 스와이퍼

--- js/main.js
+++ js/main.js
@@ -17,6 +17,15 @@ $(document).ready(function () {
     },
   });
 
+  var swiper = new Swiper(".con_2_wrap", {
+    slidesPerView: 1.8,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
   var swiper = new Swiper(".sec_5", {
     slidesPerView: 3.8,
     spaceBetween: 8,
